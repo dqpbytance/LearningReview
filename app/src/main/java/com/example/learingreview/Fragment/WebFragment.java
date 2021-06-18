cf8eba459b87fe23512f9cc8de4235a22c92ab3e
dqp <duanqipeng@bytedance.com>
2021-06-18 14:01:57 +0800

    implement first fragment and implement the backup method use handler.

--- app/src/main/java/com/example/learingreview/Fragment/WebFragment.java
+++ app/src/main/java/com/example/learingreview/Fragment/WebFragment.java
@@ -1,10 +1,17 @@
 package com.example.learingreview.Fragment;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,8 +21,22 @@ import androidx.fragment.app.Fragment;
 import com.example.learingreview.R;
 
 public class WebFragment extends Fragment {
+    private View view;
+    private WebView webView;
+    private Handler handler=new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(@NonNull Message msg) {
+            switch (msg.what){
+                case 1:
+                    webView.goBack();
+                    break;
+                default:break;
+            }
+            return true;
+        }
+    });
     public View onCreateView( LayoutInflater inflater, ViewGroup container,  Bundle savedInstanceState) {
-        View view =inflater.inflate(R.layout.fragment_web,container,false);
+        view =inflater.inflate(R.layout.fragment_web,container,false);
         return view;
     }
 
@@ -23,5 +44,33 @@ public class WebFragment extends Fragment {
     public void onResume() {
         super.onResume();
         System.out.println(getClass().toString());
+    }
+
+    @Override
+    public void onViewCreated( View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        webView = view.findViewById(R.id.webview);
+
+        WebSettings webSetting = webView.getSettings();
+        webSetting.setJavaScriptEnabled(true);
+        //webView.setWebChromeClient(new WebChromeClient());
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                view.loadUrl(request.getUrl().toString());
+                return true;
+            }
+        });
+        webView.loadUrl("https://m.baidu.com");
+        webView.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (keyCode==KeyEvent.KEYCODE_BACK&&webView.canGoBack()){
+                    handler.sendEmptyMessage(1);
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 }
